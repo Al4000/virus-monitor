206219af09bf7aab49967f84c78d575e03b2807d
Alexander <kazakov_ai87@mail.ru>
2020-04-05 16:59:38 +0300

    build

---
+++ build/precache-manifest.66b6304c8ce9f86e905282c1bb95ea07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7821c827bc443ead6023ff1e839b02e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "251a0b3bff484cdfb43b",
+    "url": "/static/css/main.0e74077f.chunk.css"
+  },
+  {
+    "revision": "f9e1262cced0f4c1617a",
+    "url": "/static/js/2.3e95e271.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3e95e271.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "251a0b3bff484cdfb43b",
+    "url": "/static/js/main.4fa183c1.chunk.js"
+  },
+  {
+    "revision": "48d6b161bcff9e260191",
+    "url": "/static/js/runtime-main.9e537003.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
