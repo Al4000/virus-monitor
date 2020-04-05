312b6ae6fe53b9cb1ef131e5587896e582e8f554
Alexander <kazakov_ai87@mail.ru>
2020-04-05 17:14:39 +0300

    Updates

---
+++ precache-manifest.d4ddcdd32adcbb5176b198210deb514f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1a0fbbd2b5d87e3c3205aa12fb9ae33",
+    "url": "/Al4000/virus-monitor/index.html"
+  },
+  {
+    "revision": "0d3016800718789154f7",
+    "url": "/Al4000/virus-monitor/static/css/main.0e74077f.chunk.css"
+  },
+  {
+    "revision": "d1c03d7ce641bf2d052d",
+    "url": "/Al4000/virus-monitor/static/js/2.70236c1e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Al4000/virus-monitor/static/js/2.70236c1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d3016800718789154f7",
+    "url": "/Al4000/virus-monitor/static/js/main.8bdc98fa.chunk.js"
+  },
+  {
+    "revision": "5a3cd0550dc07284d6a6",
+    "url": "/Al4000/virus-monitor/static/js/runtime-main.a8357b59.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Al4000/virus-monitor/static/media/logo.5d5d9eef.svg"
+  }
+]);
